42ae84043455d46ea1481d41577c31d409f008f1
ajalba <ajalba@correo.ugr.es>
2021-10-18 17:56:20 +0200

    añadida descripcion a clase plantilla closes #14

--- src/usuario.ts
+++ src/usuario.ts
@@ -1,4 +1,11 @@
 interface Usuario{
+    /*
+    Cada usuario tendrá un id unico y sera necesario al menos tener 
+    nombre
+    correo
+
+
+    */
     //CRUD convencional
     getUsuarios();
     getUsuario();
